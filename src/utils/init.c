93a496a8ca895fe12f296a77a3bcea7d234fd72c
Nobunaga-Z <leqso.davitashvili.st@gmail.com>
2026-02-25 19:02:30 +0100

    added init_player()

--- src/utils/init.c
+++ src/utils/init.c
@@ -6,7 +6,7 @@
 /*   By: adavitas <adavitas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/03 16:21:40 by zzhyrgal          #+#    #+#             */
-/*   Updated: 2026/02/25 18:16:13 by adavitas         ###   ########.fr       */
+/*   Updated: 2026/02/25 19:01:07 by adavitas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,39 @@
 // all bools false. No need to set fields individually afterwards.
 // Answering your question: setting a freed pointer to NULL before a
 // second free() call IS safe — free(NULL) is a no-op by the C standard.
+
+void    init_player(t_game *game)
+{
+    t_player    *p;
+
+    p = &game->player;
+
+    // Center of tile: tile index 0 occupies [0.0, 1.0], so center is 0.5
+    p->pos_x = p->x + 0.5;
+    p->pos_y = p->y + 0.5;
+
+    if (p->dir == 'N')
+    {
+        p->dir_x = 0;    p->dir_y = -1;
+        p->plane_x = 0.66; p->plane_y = 0;
+    }
+    else if (p->dir == 'S')
+    {
+        p->dir_x = 0;    p->dir_y = 1;
+        p->plane_x = -0.66; p->plane_y = 0;
+    }
+    else if (p->dir == 'E')
+    {
+        p->dir_x = 1;    p->dir_y = 0;
+        p->plane_x = 0;  p->plane_y = 0.66;
+    }
+    else if (p->dir == 'W')
+    {
+        p->dir_x = -1;   p->dir_y = 0;
+        p->plane_x = 0;  p->plane_y = -0.66;
+    }
+}
+
 void init_game(t_game *game)
 {
     memset(game, 0, sizeof(t_game));
